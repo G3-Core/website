74e3f9e22df85a3046b60ab32386dde121249c7b
gustavobm <g.bianchinimoraes@gmail.com>
2025-03-05 14:41:27 -0300

    Add GitHub Actions workflow for deployment and configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/g3tech.github.io/',
+  server: {
+    open: true,
+    port: 3001
+  }
 })
