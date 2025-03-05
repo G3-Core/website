f0f6f2908e7ad674ae3ae9448478944a02b5b04c
gustavobm <g.bianchinimoraes@gmail.com>
2025-03-05 15:00:29 -0300

    Update base path in Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/g3tech.github.io/',
+  base: '/g3tech/',
   server: {
     open: true,
     port: 3001
